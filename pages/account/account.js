7b5e60db1c098068e6af85969842e2329e8f1ddc
DonaLd-D <1376825153@qq.com>
2021-08-22 22:51:13 +0800

    账单输入弹出框
    账单输入弹出框

--- pages/account/account.js
+++ pages/account/account.js
@@ -1,18 +1,72 @@
-// pages/account/account.js
+import request from '../../utils/request'
+import Dialog from '../../miniprogram_npm/@vant/weapp/dialog/dialog';
+let app=getApp()
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    show:false,
+    showOutputTab:true,
+    outputTab:[],
+    incomeTab:[],
+    outputActiveId:1,
+    incomeActiveId:11,
+    showTextarea:false
   },
-
+  handleOpen(){
+    this.setData({show:true})
+  },
+  handleClose(){
+    this.setData({show:false})
+  },
+  handleTab(){
+    this.setData({
+      showOutputTab:!this.data.showOutputTab
+    })
+  },
+  handleActive(e){
+    const {id,type}=e.target.dataset
+    if(type=='1'){
+      this.setData({
+        outputActiveId:id
+      })
+    }else if(type=='2'){
+      this.setData({
+        incomeActiveId:id
+      })
+    }
+  },
+  handleTextarea(){
+    const status=!this.data.showTextarea
+    this.setData({
+      showTextarea:status
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    request({
+      url:'/api/type/list',
+      header:{
+        'Authorization':app.globalData.token
+      }
+    }).then(res=>{
+      console.log(res)
+      if(res.data.code==200){
+        const {list}=res.data.data
+        this.setData({
+          outputTab:list.slice(0,10),
+          incomeTab:list.slice(10)
+        })
+      }else{
+        Dialog.alert({
+          message: res.data.msg,
+        })
+      }
+      
+    })
   },
 
   /**
