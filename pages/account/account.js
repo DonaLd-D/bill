d8ed9e44a1df6277bea962b2121714a8e07a752b
DonaLd-D <1376825153@qq.com>
2021-08-28 22:25:24 +0800

    添加记账

--- pages/account/account.js
+++ pages/account/account.js
@@ -1,46 +1,154 @@
 import request from '../../utils/request'
 import Dialog from '../../miniprogram_npm/@vant/weapp/dialog/dialog';
+import {getYearMonth,getMonthDay} from '../../utils/util'
 let app=getApp()
+
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-    show:false,
+    popupShow:false,
     showOutputTab:true,
     outputTab:[],
     incomeTab:[],
     outputActiveId:1,
     incomeActiveId:11,
-    showTextarea:false
+    typeName:'',
+    showTextarea:false,
+    selectDate:false,
+    currentYear:getYearMonth(new Date()),
+    currentMonth:getMonthDay(new Date()),
+    currentDate: new Date().getTime(),
+    formatter(type, value) {
+      if (type === 'year') {
+        return `${value}年`;
+      } 
+      if (type === 'month') {
+        return `${value}月`;
+      }
+      return value;
+    },
+    cashValue:null,
+    textValue:null
   },
   handleOpen(){
-    this.setData({show:true})
+    this.setData({popupShow:true})
   },
   handleClose(){
-    this.setData({show:false})
+    this.setData({popupShow:false})
   },
+  //选择记账类型，支出还是收入
   handleTab(){
     this.setData({
       showOutputTab:!this.data.showOutputTab
     })
   },
+  //选择消费类型
   handleActive(e){
-    const {id,type}=e.target.dataset
+    const {id,type,name}=e.target.dataset
     if(type=='1'){
       this.setData({
-        outputActiveId:id
+        outputActiveId:id,
+        typeName:name
       })
     }else if(type=='2'){
       this.setData({
-        incomeActiveId:id
+        incomeActiveId:id,
+        typeName:name
       })
     }
   },
-  handleTextarea(){
-    const status=!this.data.showTextarea
+  //展示选择日期弹出框
+  handleDate(){
     this.setData({
-      showTextarea:status
+      selectDate:!this.data.selectDate
+    })
+  },
+
+  //每次选择日期时触发
+  handleDateInput(e){
+    // console.log(e)
+    let {detail}=e
+    if(this.data.popupShow){
+      this.setData({
+        currentDate: detail,
+        currentMonth:getMonthDay(new Date(detail))
+      });
+    }else{
+      this.setData({currentYear:getYearMonth(new Date(detail))})
+    }
+  },
+  //确定选择日期
+  handleDateConfirm(e){
+    this.setData({
+      selectDate:!this.data.selectDate
+    })
+  },
+  //取消选择日期
+  handleDateCancel(){
+    if(this.data.popupShow){
+      this.setData({
+        currentDate:new Date().getTime(),
+        currentMonth:getMonthDay(new Date())
+      })
+    }else{
+      this.setData({currentYear:getYearMonth(new Date())})
+    }
+    this.setData({
+      selectDate:!this.data.selectDate
+    })
+  },
+  //输入金额事件
+  handleCash(e){
+    console.log(e)
+    let {value}=e.detail
+    if(value){
+      this.setData({
+        cashValue:Number(value)
+      })
+    }
+  },
+  //文本框输入事件
+  handleText(e){
+    console.log(e)
+    let {value}=e.detail
+    if(value){
+      this.setData({
+        textValue:value
+      })
+    }
+  },
+  //发送记账信息至后端
+  handleBtn(){
+    if(!this.data.cashValue){
+      Dialog.alert({
+        message: '请输入收入或支出金额',
+      })
+    }
+    request({
+      url:'/api/bill/add',
+      method:'POST',
+      data:{
+        amount:this.data.cashValue,
+        type_id:this.data.showOutputTab?this.data.outputActiveId:this.data.incomeActiveId,
+        type_name:this.data.typeName,
+        date:this.data.currentDate,
+        pay_type:this.data.outputActiveId?1:2,
+        remark:this.data.textValue
+      },
+      header:{
+        'content-type':'application/x-www-form-urlencoded',
+        'Authorization':app.globalData.token
+      }
+    }).then(res=>{
+      if(res.data.code==200){
+        this.handleClose()
+      }else{
+        Dialog.alert({
+          message: res.data.msg,
+        })
+      }
     })
   },
   /**
@@ -73,7 +181,7 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+  
   },
 
   /**
--- utils/util.js
+++ utils/util.js
@@ -1,4 +1,5 @@
-const formatTime = date => {
+//获取年和月
+const getYearMonth = date => {
   const year = date.getFullYear()
   const month = date.getMonth() + 1
   const day = date.getDate()
@@ -6,7 +7,7 @@ const formatTime = date => {
   const minute = date.getMinutes()
   const second = date.getSeconds()
 
-  return `${[year, month, day].map(formatNumber).join('/')} ${[hour, minute, second].map(formatNumber).join(':')}`
+  return `${[year, month].map(formatNumber).join('-')}`
 }
 
 const formatNumber = n => {
@@ -14,6 +15,22 @@ const formatNumber = n => {
   return n[1] ? n : `0${n}`
 }
 
+//获取月和日
+const getMonthDay=(date)=>{ 
+  var strDay = date.getDate();  
+  var strMonth = date.getMonth()+1;
+  if(strMonth<10)  
+  {  
+     strMonth="0"+strMonth;  
+  }
+  if(strDay<10)  
+  {  
+     strDay="0"+strDay;  
+  }
+  return strMonth+"-"+strDay;
+}
+
 module.exports = {
-  formatTime
+  getYearMonth,
+  getMonthDay
 }
